20908b9b8204d2d5f3aaa07824d3108c2142a16c
syamimhazmi <syamimhazmi@gmail.com>
2024-11-05 00:02:19 +0700

    wrap result into match statement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{self, BufRead, BufReader}};
+use std::{fs::File, io::{self, BufRead, BufReader, Read}};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -14,8 +14,14 @@ fn main() -> io::Result<()> {
     let args = Cli::parse();
 
     // open a file and create a buffer reader
-    let file = File::open(&args.path)?;
-    let reader = BufReader::new(file);
+    let file = File::open(&args.path).expect("no such file");
+    let mut reader: BufReader<File> = BufReader::new(file);
+    let mut str = String::new();
+    let result = reader.read_to_string(&mut str);
+    match result {
+        Ok(content) => { println!("File content: {}", content) },
+        Err(error) => { println!("Oh noes: {}", error) }
+    }
 
     for (_line_num, line) in reader.lines().enumerate() {
         let line = line?;
