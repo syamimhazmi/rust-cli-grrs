d285ce0d92385fc28035eb2be475eae9ae55a99a
syamimhazmi <syamimhazmi@gmail.com>
2024-11-06 05:21:11 +0700

    add logger

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{fs::File, io::{BufReader, Read}};
 use clap::Parser;
 use anyhow::{Context, Result};
+use log::{error, info};
 
 #[derive(Parser)]
 #[command(author="Syamim Hazmi", version, about="Search for patterns in file")]
@@ -9,18 +10,39 @@ struct Cli {
     pattern: String,
     #[arg(help="The file path to search into")]
     path: std::path::PathBuf,
+    #[command(flatten)]
+    verbose: clap_verbosity_flag::Verbosity
 }
 
 fn main() -> Result<()> {
+    env_logger::init();
+
     let args = Cli::parse();
 
+    info!("starting up");
+
     // open a file and create a buffer reader
-    let file = File::open(&args.path).expect("no such file");
+    let file = match File::open(&args.path) {
+        Ok(f) => f,
+        Err(e) => {
+            error!("failed to open a file '{}': {}", args.path.display(), e);
+            return  Err(e).with_context(
+                || format!("failed to open file `{}`", args.path.display())            
+            );
+        }
+    };
     let mut reader: BufReader<File> = BufReader::new(file);
     let mut content = String::new();
     // read the buffer reader and write in into String
-    reader.read_to_string(&mut content)
-        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+    match reader.read_to_string(&mut content) {
+        Ok(_) => (),
+        Err(e) => {
+            error!("failed to read file '{}': {}", args.path.display(), e);
+            return Err(e).with_context(
+                || format!("could not read file `{}`", args.path.display())
+            )
+        }
+    }
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
